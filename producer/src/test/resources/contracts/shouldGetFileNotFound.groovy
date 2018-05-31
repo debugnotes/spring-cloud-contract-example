9f5975fc99f121e5f083be424049956452d46612
debugnotes <brnhndrsn@gmail.com>
2018-05-31 11:41:00 -0700

    Added initial contracts. (#2)

---
+++ producer/src/test/resources/contracts/shouldGetFileNotFound.groovy
@@ -0,0 +1,22 @@
+org.springframework.cloud.contract.spec.Contract.make {
+	description("""
+Bad URL request.
+
+```
+given:
+	bad url
+when:
+	calling bad url
+then:
+	respond with file not found error
+```
+
+""")
+	request {
+		method 'GET'
+		url '/some-unknown-url'
+	}
+	response {
+		status 404
+	}
+}
---
+++ producer/src/test/resources/contracts/shouldGetOK.groovy
@@ -0,0 +1,22 @@
+
+org.springframework.cloud.contract.spec.Contract.make {
+	description("""
+Service online check.
+```
+given:
+	a motive to test the service
+when:
+	calling the service
+then:
+	respond with an ok message
+```
+
+""")
+	request {
+		method 'GET'
+		url '/'
+	}
+	response {
+		status 200
+	}
+}
